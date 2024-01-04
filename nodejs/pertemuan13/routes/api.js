72e8b80c827e20b7abfa738573296e7564089857
byudhau23 <bimobigas990@gmail.com>
2024-01-04 23:29:01 +0700

    pertemuan13

---
+++ nodejs/pertemuan13/routes/api.js
@@ -0,0 +1,17 @@
+const StudentController = require('../controllers/StudentController.js');
+const express = require('express');
+
+const router = express.Router();
+
+// mendefinisikan route
+router.get('/', (req, res) => {
+    res.send('Hai Bimo!');
+});
+
+// Routing student
+router.get('/students', StudentController.index);
+
+router.post('/students', StudentController.store);
+
+// export router
+module.exports = router;
