038f1b3491c6f2dc9ec5d035603ec1cfd6911e48
byudhau23 <bimobigas990@gmail.com>
2024-01-04 23:36:24 +0700

    Task Pertemuan 13

--- nodejs/pertemuan13/routes/api.js
+++ nodejs/pertemuan13/routes/api.js
@@ -10,8 +10,10 @@ router.get('/', (req, res) => {
 
 // Routing student
 router.get('/students', StudentController.index);
-
 router.post('/students', StudentController.store);
+router.put("/students/:id", StudentController.update);
+router.delete("/students/:id", StudentController.destroy);
+router.get("/students/:id", StudentController.show);
 
 // export router
 module.exports = router;
