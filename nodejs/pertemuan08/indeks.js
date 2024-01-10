6e3ca3d51dadc52fd239c174483a2198ae0521b5
byudhau23 <bimobigas990@gmail.com>
2024-01-10 19:07:30 +0700

    pertemuan08

---
+++ nodejs/pertemuan08/indeks.js
@@ -0,0 +1,35 @@
+// Variable
+let name = "Bimo Yudha Utama";
+console.log(name);
+
+// Array
+let angka = [1, 2, 3, 4, 5];
+console.log(angka[0]);
+console.log(angka.length);
+
+// Object
+let mahasiswa = {
+  nama: "Bimo Yudha Utama",
+  nim: "0110222057",
+  jurusan: "Teknik Informatika",
+};
+console.log(mahasiswa.jurusan);
+
+// If Else
+let nilai = 80;
+if (nilai >= 80) {
+  console.log("Lulus");
+} else {
+  console.log("Tidak Lulus");
+}
+
+// Looping
+for (let i = 1; i < 10; i++) {
+  console.log(i);
+}
+
+// Function
+function tambah(a, b) {
+  return a + b;
+}
+console.log(tambah(1, 2));
